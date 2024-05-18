466ef8719c39422cf550b3148d93a986282578a3
Егор Коновалов <eg.konovalov@mail.ru>
2024-05-18 19:18:47 +0300

    chore: update vigenere.py

--- vigenere.py
+++ vigenere.py
@@ -10,21 +10,13 @@ mode = 'encrypt'
 # Do not touch it
 output = ''
 
-if mode == 'encrypt':
-    for i in range(len(text)):
-        letter = text.lower()[i]
-        letter2 = secret.lower()[i % len(secret)]
-        index = alphabet.find(letter)
-        index2 = alphabet.find(letter2)
-        new_index = (index + index2) % len(alphabet)
-        output += ''.join(alphabet[new_index])
-else:
-    for i in range(len(text)):
-        letter = text.lower()[i]
-        letter2 = secret.lower()[i % len(secret)]
-        index = alphabet.find(letter)
-        index2 = alphabet.find(letter2)
-        new_index = (index - index2) % len(alphabet)
-        output += ''.join(alphabet[new_index])
+for i in range(len(text)):
+    letter = text.lower()[i]
+    letter2 = secret.lower()[i % len(secret)]
+    index = alphabet.find(letter)
+    index2 = alphabet.find(letter2)
+    if mode == 'encrypt': new_index = (index + index2) % len(alphabet)
+    else: new_index = (index - index2) % len(alphabet)
+    output += ''.join(alphabet[new_index])
 
 print(output)
