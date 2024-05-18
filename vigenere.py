c4384a11fa02d52cf0f2e319eb05f823f591c352
Егор Коновалов <eg.konovalov@mail.ru>
2024-05-18 19:02:47 +0300

    chore: update vigenere.py

--- vigenere.py
+++ vigenere.py
@@ -4,16 +4,27 @@ alphabet = 'abcdefghijklmnopqrstuvwxyz'
 secret = 'Chair'
 # Your text
 text = 'Example'
+# 'encrypt' or 'decrypt'
+mode = 'encrypt'
 
 # Do not touch it
 output = ''
 
-for i in range(len(text)):
-    letter = text.lower()[i]
-    letter2 = secret.lower()[i % len(secret)]
-    index = alphabet.find(letter)
-    index2 = alphabet.find(letter2)
-    new_index = (index + index2) % len(alphabet)
-    output += ''.join(alphabet[new_index])
+if mode == 'encrypt':
+    for i in range(len(text)):
+        letter = text.lower()[i]
+        letter2 = secret.lower()[i % len(secret)]
+        index = alphabet.find(letter)
+        index2 = alphabet.find(letter2)
+        new_index = (index + index2) % len(alphabet)
+        output += ''.join(alphabet[new_index])
+else:
+    for i in range(len(text)):
+        letter = text.lower()[i]
+        letter2 = secret.lower()[i % len(secret)]
+        index = alphabet.find(letter)
+        index2 = alphabet.find(letter2)
+        new_index = (index - index2) % len(alphabet)
+        output += ''.join(alphabet[new_index])
 
 print(output)
