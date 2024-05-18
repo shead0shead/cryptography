8052d5d04c4533fd5d96d07fee3c7253fd42f659
Егор Коновалов <eg.konovalov@mail.ru>
2024-05-18 19:02:30 +0300

    chore: update caesarianae.py

--- caesarianae.py
+++ caesarianae.py
@@ -1,11 +1,11 @@
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
-# 1 - Encrypt 2 - Decrypt
-type = 1
-# Shift step
-step = 3
 # Your text
 text = 'Example'
+# Shift step
+step = 3
+# 'encrypt' or 'decrypt'
+mode = 'encrypt'
 
 # Do not touch it
 output = ''
@@ -13,7 +13,7 @@ output = ''
 for i in range(len(text)):
     letter = text.lower()[i]
     index = alphabet.find(letter)
-    if type == 1: new_index = (index + step) % len(alphabet)
+    if mode == 'encrypt': new_index = (index + step) % len(alphabet)
     else: new_index = (index - step) % len(alphabet)
     output += ''.join(alphabet[new_index])
 
