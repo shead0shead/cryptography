02abb0fb752dc47825e167a66fb24289e6ea0ec7
Егор Коновалов <eg.konovalov@mail.ru>
2024-05-18 13:23:59 +0300

    chore: update caesarianae.py

--- caesarianae.py
+++ caesarianae.py
@@ -1,13 +1,17 @@
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
+# 1 - Encrypt 2 - Decrypt
 type = 1
+# Character shift step
 step = 3
+# Write your text here
+text = 'Example'
 
-input = 'Example'
+# Do not touch it
 output = ''
 
-for i in range(len(input)):
-    letter = input.lower()[i]
+for i in range(len(text)):
+    letter = text.lower()[i]
     index = alphabet.find(letter)
     if type == 1: new_index = (index + step) % len(alphabet)
     else: new_index = (index - step) % len(alphabet)
